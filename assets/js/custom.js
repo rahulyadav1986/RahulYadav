638bb13fe255598d8e0595a8417ff8852a06eb71
rahulyadav1986 <rahullooksforward@gmail.com>
2020-04-07 00:25:11 +0530

    updated

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+const newLocal = setInterval(function () {
+    jQuery('.humberger-menu').click(function(){
+        jQuery('.side-menu').addClass('open');
+        jQuery('.ovearlay').addClass('open');
+    });
+    jQuery('.menuback').click(function(){
+        jQuery('.side-menu').removeClass('open');
+        jQuery('.ovearlay').removeClass('open');
+    });
+    jQuery('.ovearlay').click(function(){
+        jQuery('.side-menu').removeClass('open');
+        jQuery('.ovearlay').removeClass('open');
+    });
+    jQuery('.nav-menu li').click(function(){
+        jQuery('.side-menu').removeClass('open');
+        jQuery('.ovearlay').removeClass('open');
+    });
+    setTimeout(function () {
+        jQuery('.load-page').hide();
+    }, 3000);
+}, 1000);
